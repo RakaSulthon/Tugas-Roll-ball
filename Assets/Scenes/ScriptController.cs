a7ea557cf1d6529c6f5491297b989ad950e225dd
Raka Sulthon <ASanrar92@gmail.com>
2018-12-31 20:56:58 +0700

    Tugas Prak Sismul

---
+++ Assets/Scenes/ScriptController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScriptController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(MoveHorizontal, 0.0f, MoveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    
+}
